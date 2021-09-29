f0159cbbd32741c0926bbe90d021a174f3361567
JacobVaerst23 <816769@mystma.org>
2021-09-29 08:43:02 -0500

    close10 attempt 3

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #WRITE YOUR CODE IN THIS FILE
 
 def close10(x, y):
-    if (x-10) < (y-10):
-        return "x"
+    if abs(x-10) < abs(y-10):
+        return x
 
-    elif (x-10) == (y-10):
-        return "0"
+    elif abs(x-10) == abs(y-10):
+        return 0
 
     else:
-        return "y"
+        return y
 
-print(close10(12, 5))
+print(close10(16, 5))
 
     
         
